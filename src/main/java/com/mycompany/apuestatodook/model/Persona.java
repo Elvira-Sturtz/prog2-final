1aa63e39670c243ddcd177f3f7d83427ae5a473b
Elvira <elvira.sturtz@alu.inspt.utn.edu.ar>
2024-12-14 20:15:32 -0300

    anotaciones de mapeo clase Persona

--- src/main/java/com/mycompany/apuestatodook/model/Persona.java
+++ src/main/java/com/mycompany/apuestatodook/model/Persona.java
@@ -1,19 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.apuestatodook.model;
 
-/**
- *
- * @author Tango
- */
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table; 
+
+
+@Entity
+@Table (name="Persona") 
 public class Persona {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY) 
     private int id_persona;
+    
+    @Column(length=255)
     private String dni;
+    @Column(length=255)
     private String nombre;
+    @Column(length=255)
     private String apellido;
     private int edad;
+    
+    @Column(name="fk_id_usuario")
     private int fk_id_ususario;
 
     public String getDni() {
@@ -46,6 +58,18 @@ public class Persona {
 
     public void setEdad(int edad) {
         this.edad = edad;
+    }
+
+    public Persona() {
+    }
+    
+    
+    public Persona(int idUsuario, String nombre, String apellido, int edad, String dni){
+        this.fk_id_ususario = idUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dni = dni;
     }
 
     public Persona(int id_persona, String dni, String nombre, String apellido, int edad, int fk_id_ususario) {
