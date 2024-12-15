7af6b32dbce31f2aa6cbc6d3ad19324e53d213bc
Elvira <elvira.sturtz@alu.inspt.utn.edu.ar>
2024-12-15 01:08:15 -0300

    ApuestaDAO metodo getAllApuestasConResultado con hibernate

--- src/main/java/com/mycompany/apuestatodook/model/ApuestaDAO.java
+++ src/main/java/com/mycompany/apuestatodook/model/ApuestaDAO.java
@@ -88,31 +88,36 @@ public class ApuestaDAO {
         }
     }
     
+      
     public List<Apuesta> getAllApuestasConResultado() {
-    List<Apuesta> apuestasConResultado = new ArrayList<>();
-    String query = "SELECT apuesta.por_quien, apuesta.monto, partido.local, partido.visitante, partido.fecha " +
-            "FROM apuesta " +
-            "JOIN resultado ON resultado.fk_id_partido = apuesta.fk_id_partido " +
-            "JOIN partido ON partido.id_partido = apuesta.fk_id_partido";
-
-    try (Connection con = ConnectionPool.getInstance().getConnection();
-         PreparedStatement ps = con.prepareStatement(query);
-         ResultSet rs = ps.executeQuery()) {
-        while (rs.next()) {
-            String por_quien = rs.getString("por_quien");
-            int monto = rs.getInt("monto");
-            String local = rs.getString("local");
-            String visitante = rs.getString("visitante");
-            String fecha = rs.getString("fecha");
-
-            Apuesta apuesta = new Apuesta(local, visitante, fecha, monto, por_quien);
-            apuestasConResultado.add(apuesta);
+        List<Apuesta> apuestasConResultado = new ArrayList<>();
+        try(Session session = HibernateUtil.getSessionFactory().getCurrentSession() ){
+            session.beginTransaction();
+            List<Object[]> resultados = 
+                    session.createQuery("SELECT a.por_quien, a.monto, p.local, p.visitante, p.fecha "+
+                            "FROM Apuesta as a "+
+                            "JOIN Resultado as r ON r.idPartido = a.idPartido "+
+                            "JOIN Partido as p ON p.idPartido = a.idPartido",Object[].class)
+                            .getResultList();
+            
+            for (var resultado : resultados) {
+                String por_quien = (String) resultado[0];
+                int monto = (int) resultado[1];
+                String local = (String) resultado[2];
+                String visitante = (String) resultado[3];
+                String fecha = (String) resultado[4];
+                Apuesta apuesta = new Apuesta(local, visitante, fecha, monto, por_quien);
+                apuestasConResultado.add(apuesta);
+            }
+            
+            session.getTransaction().commit();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
         }
-    } catch (SQLException ex) {
-        throw new RuntimeException(ex);
+        return apuestasConResultado;
     }
-    return apuestasConResultado;
-    }
+    
+    
     public List<Apuesta> getApuestasConResultadoPorUsuario(int idUsuario) {
     List<Apuesta> apuestasConResultado = new ArrayList<>();
     String query = "SELECT apuesta.por_quien, apuesta.monto, partido.local, partido.visitante, partido.fecha " +
